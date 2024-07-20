42f29854868388da7d48167ec093c887ece217ad
shibincreji <scr2404@gmail.com>
2024-07-20 16:05:40 +0530

    Blog Page v2

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -13,7 +13,7 @@ const NavLinks = () => {
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/#portfolio">
                 Portfolio
             </HashLink> */}
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/blog#blog">
+            <HashLink className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" to="/blog#blog">
             Insights
             </HashLink> 
             {/* <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact"> */}
